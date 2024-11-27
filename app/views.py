cd74bad96a3c43896e6fb4b0e4a0952eaef8d184
MattTHP <cerberogt98@hotmail.com>
2024-11-27 11:31:47 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.core.paginator import Paginator
 from django.shortcuts import render
 from app.layers.persistence import repositories
 
